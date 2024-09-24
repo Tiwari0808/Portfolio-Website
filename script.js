9eb32965fedcce95154bd6ad421da6331d1ade6e
Divyanshu <divyanshutiwari173@gmail.com>
2024-09-24 12:19:32 +0530

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-console.log('hello')
+console.log('hello');
 
 let tablinks = document.querySelectorAll(".tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
 opentab = (tabname, event) => {
-
     tablinks.forEach(tablink => {
         tablink.classList.remove("active-link");
     });
@@ -15,20 +14,40 @@ opentab = (tabname, event) => {
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
-}
+};
+
 
 let xMark = document.querySelector('.fa-solid');
 let menuIcon = document.querySelector('.fa-bars');
 let sidemenu = document.querySelector('#sidemenu');
 
 function openMenu(){
-sidemenu.style.right = '0';
-// console.log('open')
-
+    sidemenu.style.right = '0';
 }
 
-closeMenu =()=>{
+closeMenu = () => {
     sidemenu.style.right = '-40%';
-    // console.log('close')
+};
 
-    }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyKE8sPjkKmLzQhAiPQABpevxHdgL7XR4waeLC3iUM52FNykgd-yjT7194HcIaA-zgS/exec';
+const form = document.forms['submit-to-google-sheet'];
+const formResponse = document.querySelector('.form-response');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        formResponse.innerHTML = 'Form submitted successfully!';
+        form.reset(); 
+    })
+    .catch(error => {
+        formResponse.innerHTML = 'Form submitted successfully!';
+    });
+});
+
+
+
+
